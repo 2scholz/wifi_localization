0bb22f4703a9c88f779b33b936c95c1dc85e2aa9
2scholz <2scholz@informatik.uni-hamburg.de>
2016-07-27 16:53:06 +0200

    Some bug fixes for the occupancy grid. Also the resolution is now variable.

--- wifi_localization/src/wifi_data_collector_node.cpp
+++ wifi_localization/src/wifi_data_collector_node.cpp
@@ -35,7 +35,7 @@ struct Map_data{
 class Subscriber
 {
 public:
-  Subscriber(ros::NodeHandle &n, double threshold, bool user_input);
+  Subscriber(ros::NodeHandle &n, double threshold, bool user_input, float map_resolution);
   void recordNext();
   bool& isRecording();
 
@@ -55,11 +55,12 @@ private:
   double pos_y_;
   double max_weight_;
   nav_msgs::OccupancyGrid amcl_map_;
+  nav_msgs::OccupancyGrid my_map_;
 
   // TODO: delete the variables that aren't used.
   int map_height_;
   int map_width_;
-  double map_resolution_;
+  float map_resolution_;
   double map_origin_x_;
   double map_origin_y_;
 
@@ -75,8 +76,8 @@ private:
   void wifiCallbackMethod(const wifi_localization::WifiState::ConstPtr& wifi_data_msg);
 };
 
-Subscriber::Subscriber(ros::NodeHandle &n, double threshold, bool user_input) :
-  threshold_(threshold), user_input_(user_input), record_(false), n_(n)
+Subscriber::Subscriber(ros::NodeHandle &n, double threshold, bool user_input, float map_resolution) :
+  threshold_(threshold), user_input_(user_input), record_(false), n_(n), map_resolution_(map_resolution)
 {
   ROS_INFO("Threshold at: %f",threshold_);
   time_t rawtime;
@@ -127,14 +128,26 @@ Subscriber::Subscriber(ros::NodeHandle &n, double threshold, bool user_input) :
     ROS_ERROR("Failed to call map service");
     return;
   }
+  my_map_ = amcl_map_;
+  my_map_.info.resolution = map_resolution_;
+  my_map_.info.width = (unsigned int)((amcl_map_.info.width * amcl_map_.info.resolution)/map_resolution_);
+  my_map_.info.height = (unsigned int)((amcl_map_.info.height * amcl_map_.info.resolution)/map_resolution_);
+  my_map_.data.resize(my_map_.info.width * my_map_.info.height);
 
-  //amcl_map_.data.erase(amcl_map_.data.begin(), amcl_map_.data.end());
-  std::fill(amcl_map_.data.begin(), amcl_map_.data.end(), 0);
+  /*
+  std::fill(amcl_map_.data.begin(), amcl_map_.data.end(), -1);
   map_height_ = amcl_map_.info.height;
   map_width_ = amcl_map_.info.width;
   map_resolution_ = amcl_map_.info.resolution;
   map_origin_x_ = amcl_map_.info.origin.position.x;
   map_origin_y_ = amcl_map_.info.origin.position.y;
+   */
+
+  std::fill(my_map_.data.begin(), my_map_.data.end(), -1);
+  map_height_ = my_map_.info.height;
+  map_width_ = my_map_.info.width;
+  map_origin_x_ = my_map_.info.origin.position.x;
+  map_origin_y_ = my_map_.info.origin.position.y;
 }
 
 void Subscriber::recordNext()
@@ -154,6 +167,7 @@ void Subscriber::amclCallbackMethod(const wifi_localization::MaxWeight::ConstPtr
   pos_x_ = pose_msg->pose.pose.position.x;
   pos_y_ = pose_msg->pose.pose.position.y;
   max_weight_ = max_weight_msg->max_weight;
+  std::cout << "pos_x: " << pos_x_ << std::endl;
 }
 
 void Subscriber::wifiCallbackMethod(const wifi_localization::WifiState::ConstPtr& wifi_data_msg)
@@ -166,58 +180,40 @@ void Subscriber::wifiCallbackMethod(const wifi_localization::WifiState::ConstPtr
       std::string mac_name = wifi_data_msg->macs.at(i);
       double wifi_dbm = wifi_data_msg->strengths.at(i);
 
-      // std::map<std::string, boost::shared_ptr<std::ofstream> >::iterator file = filemap_.find(mac_name);
-
-      // std::map<std::string, std::pair<nav_msgs::OccupancyGrid, ros::Publisher> >::iterator map = wifi_map_pubs_.find(mac_name);
-
       std::map<std::string, Map_data>::iterator data = mac_map_.find(mac_name);
 
-      // If the file for this mac address doesn't exist yet, it is going to be created.
-      /*
-      if (file == filemap_.end())
-      {
-        boost::shared_ptr<std::ofstream> new_mac = boost::make_shared<std::ofstream>();
-        new_mac->open(std::string("./wifi_data/" + date_ + "/" + mac_name + ".csv").c_str());
-        *new_mac << "x, y, strengths" << "\n";
-        file = filemap_.insert(filemap_.begin(), std::make_pair(mac_name, new_mac));
-
-        ros::Publisher wifi_map_pub = n_.advertise<nav_msgs::OccupancyGrid>(mac_name, 1000);
-        map = wifi_map_pubs_.insert(wifi_map_pubs_.begin(),
-                                    std::make_pair(mac_name, std::make_pair(amcl_map_, wifi_map_pub)));
-      }
-      */
+      // If there is no entry for this mac address yet, it is going to be created.
       if(data == mac_map_.end())
       {
         boost::shared_ptr<std::ofstream> new_mac = boost::make_shared<std::ofstream>();
         new_mac->open(std::string("./wifi_data/" + date_ + "/" + mac_name + ".csv").c_str());
         *new_mac << "x, y, strengths" << "\n";
-        // file = filemap_.insert(filemap_.begin(), std::make_pair(mac_name, new_mac));
 
-        ros::Publisher wifi_map_pub = n_.advertise<nav_msgs::OccupancyGrid>(mac_name, 1000);
-        // map = wifi_map_pubs_.insert(wifi_map_pubs_.begin(), std::make_pair(mac_name, std::make_pair(amcl_map_, wifi_map_pub)));
+        // ros won't allow topics with ':' in them, so they have to be replaced in the mac address.
+        std::string mac_pub_name = "/" + mac_name;
+        std::replace( mac_pub_name.begin(), mac_pub_name.end(), ':', '_');
+
+        ros::Publisher wifi_map_pub = n_.advertise<nav_msgs::OccupancyGrid>(mac_pub_name, 1000, true);
 
         Map_data temp;
         temp.file_ = new_mac;
-        temp.map_ = amcl_map_;
+        temp.map_ = my_map_;
         temp.pub_ = wifi_map_pub;
         data = mac_map_.insert(mac_map_.begin(), std::make_pair(mac_name, temp));
       }
 
-      // *(file->second) << pos_x_ << "," << pos_y_ << "," << wifi_dbm << "\n";
-
       *(data->second).file_ << pos_x_ << "," << pos_y_ << "," << wifi_dbm << "\n";
-
-      //file->second->flush();
 
       data->second.file_->flush();
 
       // Find the right cell, to insert the wifi data into.
-      // std::vector<int8_t>::iterator cell = map->second.first.data.begin();
-      std::vector<int8_t>::iterator cell = data->second.map_.data.begin();
-      std::advance(cell, int((pos_x_/map_resolution_) + (pos_y_*map_width_)));
+      int grid_x = (int)((pos_x_ - data->second.map_.info.origin.position.x) / data->second.map_.info.resolution);
+      int grid_y = (int)((pos_y_ - data->second.map_.info.origin.position.y) / data->second.map_.info.resolution);
+
+      int cell_pos = grid_x + (grid_y*map_width_);
 
       // convert the dbm values of the wifi strength to percentage values.
-      int quality;
+      int8_t quality;
 
       if(wifi_dbm <= -100)
         quality = 0;
@@ -226,14 +222,22 @@ void Subscriber::wifiCallbackMethod(const wifi_localization::WifiState::ConstPtr
       else
         quality = 2 * (int(wifi_dbm) + 100);
 
-      // insert the wifi-signal-quality into the occupancy grid
-      // map->second.first.data.insert(cell, quality);
-      data->second.map_.data.insert(cell, quality);
+
+      data->second.map_.data.at(cell_pos) = quality;
+
+      /*
+      std::cout << "quality: " << int(quality) << std::endl;
+      int count = 0;
+      for(std::vector<int8_t>::iterator j = data->second.map_.data.begin(); j < data->second.map_.data.end(); j++)
+      {
+        if(*j != -1)
+          count++;
+      }
+      std::cout << "count: " << count << std::endl;
+       */
 
       // publish the occupancy grid
-      // map->second.second.publish(map->second.first);
       data->second.pub_.publish(data->second.map_);
-
     }
     if(record_)
     {
@@ -293,7 +297,7 @@ int main(int argc, char **argv)
   n.param("/wifi_data_collector/threshold", threshold, threshold);
   n.param("/wifi_data_collector/user_input", user_input, user_input);
 
-  Subscriber *sub = new Subscriber(n, threshold, user_input);
+  Subscriber *sub = new Subscriber(n, threshold, user_input, 0.10);
 
   // If user-input mode is deactivated there is no need to check for user input.
   if(!user_input)
