9e81895b2f991de7a712f1fee4b368825a7e7b77
2scholz <2scholz@informatik.uni-hamburg.de>
2016-08-11 01:15:42 +0200

    Added the possibility to traverse the map in an ordered way.

--- wifi_localization/src/map_traverser_node.cpp
+++ wifi_localization/src/map_traverser_node.cpp
@@ -19,6 +19,14 @@ std::pair<double, double> fRand(double p1_x, double p1_y, double p2_x, double p2
   return std::make_pair(p_x, p_y);
 }
 
+std::pair<double, double> step_vector(double p1_x, double p1_y, double p2_x, double p2_y, double p3_x, double p3_y,
+                                      double step_length)
+{
+  double step_right = sqrt(step_length)/(pow((p2_x-p1_x),2)+pow(p2_y - p1_y,2));
+  double step_down = sqrt(step_length)/(pow((p3_x-p1_x),2)+pow(p3_y - p1_y,2));
+  return std::make_pair(step_right, step_down);
+}
+
 class DataRecorded
 {
 public:
@@ -47,12 +55,17 @@ int main(int argc, char **argv)
   double p3_x = 0.0;
   double p3_y = 0.0;
 
+  bool ordered = false;
+  double step_size = 0.20;
+
   n.param("/map_traverser/p1_x", p1_x, p1_x);
   n.param("/map_traverser/p1_y", p1_y, p1_y);
   n.param("/map_traverser/p2_x", p2_x, p2_x);
   n.param("/map_traverser/p2_y", p2_y, p2_y);
   n.param("/map_traverser/p3_x", p3_x, p3_x);
   n.param("/map_traverser/p3_y", p3_y, p3_y);
+  n.param("/map_traverser/ordered", ordered, ordered);
+  n.param("/map_traverser/step_size", step_size, step_size);
 
   if(p1_x == 0.0 && p1_y == 0.0 && p2_x == 0.0 && p2_y == 0.0 && p3_x == 0.0 && p3_y == 0.0)
   {
@@ -67,25 +80,115 @@ int main(int argc, char **argv)
   DataRecorded data_recorded;
   ros::Subscriber data_recorded_sub = n.subscribe("recorded_since_stop", 1000, &DataRecorded::callback, &data_recorded);
 
-  while(ros::ok())
+  if(ordered)
   {
-    if(data_recorded.data_recorded)
-    {
-      std::pair<double, double> goal_point = fRand(p1_x, p1_y, p2_x, p2_y, p3_x, p3_y);
-      double pos_x = goal_point.first;
-      double pos_y = goal_point.second;
-      move_base_msgs::MoveBaseGoal goal;
-      goal.target_pose.header.frame_id = "/map";
-      goal.target_pose.header.stamp = ros::Time::now();
-      goal.target_pose.pose.position.x = pos_x;
-      goal.target_pose.pose.position.y = pos_y;
-      goal.target_pose.pose.orientation.w = 1.0;
-      ac.sendGoal(goal);
+    std::pair<double, double> steps = step_vector(p1_x, p1_y, p2_x, p2_y, p3_x, p3_y, 0.20);
+    double step_right_x = steps.first * (p2_x - p1_x);
+    double step_right_y = steps.first * (p2_y - p1_y);
+    double step_down_x = steps.second * (p3_x - p1_x);
+    double step_down_y = steps.second * (p3_y - p1_y);
+    double step_right_angle = acos(step_right_x/sqrt(pow(step_right_x,2)+pow(step_right_y,2)));
+    double step_down_angle = acos(step_right_x/sqrt(pow(step_down_x,2)+pow(step_down_y,2)));
 
-      ac.waitForResult();
+    double vector_right_length = sqrt(pow((p2_x - p1_x),2) + pow((p2_y - p1_y),2));
+    double vector_down_lenght = sqrt(pow((p3_x - p1_x),2) + pow((p3_y - p1_y),2));
+    double angle = 0.0;
+
+    if(vector_right_length > vector_down_lenght)
+      angle = step_right_angle;
+    else
+      angle = step_down_angle;
+
+    // First drive to the start point, which is the upper left corner of the square.
+    double pos_x = p1_x;
+    double pos_y = p1_y;
+    move_base_msgs::MoveBaseGoal goal;
+    goal.target_pose.header.frame_id = "/map";
+    goal.target_pose.header.stamp = ros::Time::now();
+    goal.target_pose.pose.position.x = pos_x;
+    goal.target_pose.pose.position.y = pos_y;
+    goal.target_pose.pose.orientation.w = angle;
+    ac.sendGoal(goal);
+    ac.waitForResult();
+
+    int sign = 1;
+
+    while(ros::ok())
+    {
+      if(data_recorded.data_recorded)
+      {
+        if(vector_right_length > vector_down_lenght)
+        {
+          if((pos_x + step_right_x) > (p1_x +(p2_x - p1_x) + (p3_x - p1_x)) || (pos_y+step_right_y) > (p1_y+(p2_y - p1_y)+(p3_y - p1_y)))
+          {
+            if((pos_x + step_down_x) > (p1_x +(p2_x - p1_x) + (p3_x - p1_x)) || (pos_y+step_down_y) > (p1_y+(p2_y - p1_y)+(p3_y - p1_y)))
+            {
+              ROS_INFO("Map was fully traversed.");
+              return 0;
+            }
+            pos_x = pos_x + step_down_x;
+            pos_y = pos_y + step_down_y;
+            sign = -1 * sign;
+          }
+          else
+          {
+            pos_x = pos_x + sign * step_right_x;
+            pos_y = pos_y + sign * step_right_y;
+          }
+        }
+        else
+        {
+          if((pos_x + step_down_x) > (p1_x +(p2_x - p1_x) + (p3_x - p1_x)) || (pos_y+step_down_y) > (p1_y+(p2_y - p1_y)+(p3_y - p1_y)))
+          {
+            if((pos_x + step_right_x) > (p1_x +(p2_x - p1_x) + (p3_x - p1_x)) || (pos_y+step_right_y) > (p1_y+(p2_y - p1_y)+(p3_y - p1_y)))
+            {
+              ROS_INFO("Map was fully traversed.");
+              return 0;
+            }
+            pos_x = pos_x + step_down_x;
+            pos_y = pos_y + step_down_y;
+            sign = -1 * sign;
+          }
+          else
+          {
+            pos_x = pos_x + sign * step_down_x;
+            pos_y = pos_y + sign * step_down_y;
+          }
+        }
+        move_base_msgs::MoveBaseGoal goal;
+        goal.target_pose.header.frame_id = "/map";
+        goal.target_pose.header.stamp = ros::Time::now();
+        goal.target_pose.pose.position.x = pos_x;
+        goal.target_pose.pose.position.y = pos_y;
+        goal.target_pose.pose.orientation.w = angle;
+        ac.sendGoal(goal);
+        ac.waitForResult();
+      }
+      ros::spinOnce();
+      r.sleep();
     }
-    ros::spinOnce();
-    r.sleep();
+  }
+  else
+  {
+    while(ros::ok())
+    {
+      if(data_recorded.data_recorded)
+      {
+        std::pair<double, double> goal_point = fRand(p1_x, p1_y, p2_x, p2_y, p3_x, p3_y);
+        double pos_x = goal_point.first;
+        double pos_y = goal_point.second;
+        move_base_msgs::MoveBaseGoal goal;
+        goal.target_pose.header.frame_id = "/map";
+        goal.target_pose.header.stamp = ros::Time::now();
+        goal.target_pose.pose.position.x = pos_x;
+        goal.target_pose.pose.position.y = pos_y;
+        goal.target_pose.pose.orientation.w = 1.0;
+        ac.sendGoal(goal);
+        ac.waitForResult();
+      }
+      ros::spinOnce();
+      r.sleep();
+    }
   }
 
   return 0;
